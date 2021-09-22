c7d35b6900dba5f48140914e5121a730a1c02608
peng <1225376885@qq.com>
2021-09-22 18:04:07 +0800

    完成登录

---
+++ vue-shop/src/pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <!--退出按钮 -->
+        <el-button type="info" @click="logout">退出</el-button>
+    </div>
+</template>
+<script>
+export default {
+  name: 'home',
+  methods: {
+    logout () {
+      window.sessionStorage.clear('token')
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+<style>
+
+</style>
--- vue-shop/src/router/index.js
+++ vue-shop/src/router/index.js
@@ -1,27 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Login from '../pages/Login.vue'
+import Home from '../pages/Home.vue'
 Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
 const router = new VueRouter({
-  routes
+  routes: [
+    // 路由重定向 默认页面为login
+    { path: '/', redirect: '/login' },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/home',
+      component: Home
+    }
+  ]
 })
-
+router.beforeEach((to, from, next) => {
+  // console.log('beforeEnter', to, from)
+  if (to.path === '/login') return next()
+  if (!window.sessionStorage.getItem('token')) {
+    next('/login')
+  }
+  next()
+})
 export default router
